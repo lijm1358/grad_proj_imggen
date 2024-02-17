8e9b24628ee1be03c1c3236cb8742e5f14e327d9
kCMI113 <cmkang1316@gmail.com>
2024-02-17 13:28:51 +0900

    feat: modi wrong parts in cal_each in BPRMF and modi some comments

--- VBPR/src/model.py
+++ VBPR/src/model.py
@@ -2,24 +2,23 @@ import torch
 import torch.nn as nn
 
 class VBPR(nn.Module):
-    def __init__(self, n_user, n_item, K, D, img_embedding, emb_norm="L2") -> None:
+    def __init__(self, n_user, n_item, K, D, img_embedding, emb_norm="None") -> None:
         super().__init__()
-        self.feat_map= img_embedding.float() # user * 512
+        self.K = K                      # general emb dim
+        self.D = D                      # visual emb dim
         self.n_user = n_user
         self.n_item = n_item
-        self.K = K
-        self.D = D
-        self.emb_norm = emb_norm
-        self.F = self.feat_map.shape[1] 
+        self.emb_norm = emb_norm        # emb normalization method, defalut:None
+        self.feat_map = img_embedding.float() # item * F
+        self.F = self.feat_map.shape[1]       # F = 512
 
         self.offset = nn.Parameter(torch.zeros(1))
         self.user_bias = nn.Embedding(self.n_user,1)     # user*1
         self.item_bias = nn.Embedding(self.n_item,1)     # item*1
-        self.vis_bias = nn.Embedding(self.F,1)           # 512*1
+        self.vis_bias = nn.Embedding(self.F,1)           # F*1
         self.user_emb = nn.Embedding(self.n_user,self.K) # user*K
         self.item_emb = nn.Embedding(self.n_item,self.K) # item*K
-        #self.item_vis_emb = nn.Embedding(self.D, self.F) # D*F
-        self.item_vis_emb = nn.Embedding(self.F, self.D)
+        self.item_vis_emb = nn.Embedding(self.F, self.D) # F*D
         self.user_vis_emb = nn.Embedding(self.n_user, self.D) # user*D
         
         if self.emb_norm == "Batch":
@@ -56,9 +55,6 @@ class VBPR(nn.Module):
             item_emb = self.item_bn(item_emb)
             user_vis_emb = self.user_vis_bn(user_vis_emb)
             item_vis_emb = self.item_vis_bn(item_vis_emb)
-
-        '''vis_term = ((user_vis_emb).matmul(self.item_vis_emb.weight@(self.feat_map[item].T))).sum(dim=1) + (self.vis_bias.weight.T)@(self.feat_map[item].T)
-        mf_term = self.offset + self.user_bias(user).T + self.item_bias(item).T + ((user_emb).matmul(item_emb.T)).sum(dim=1).unsqueeze(dim=0)'''
         
         vis_term = ((user_vis_emb)*(self.feat_map[item]@self.item_vis_emb.weight)).sum(dim=1).unsqueeze(-1) + (self.feat_map[item])@(self.vis_bias.weight)
         mf_term = self.offset + self.user_bias(user) + self.item_bias(item) + (user_emb*item_emb).sum(dim=1).unsqueeze(-1)
@@ -87,12 +83,14 @@ class BPRLoss(nn.Module):
 
     def _reg_term(self, pos_params, neg_params):
         if self.visual:
+            # VBPR
             alpha, beta_u, beta_pos, beta_prime_pos, gamma_u, gamma_pos, e_pos, theta_u = pos_params
             _, _, beta_neg, beta_prime_neg, _, gamma_neg, e_neg, _ = neg_params
             reg_out = self.reg_theta * self._cal_l2(alpha, beta_u, beta_pos, beta_neg, theta_u, gamma_u, gamma_pos, gamma_neg)
             reg_out += self.reg_beta * self._cal_l2(beta_prime_pos, beta_prime_neg)
             reg_out += self.reg_e * self._cal_l2(e_pos, e_neg)
         else:
+            # BPR
             alpha, beta_u, beta_pos, gamma_u, gamma_pos = pos_params
             _, _, beta_neg, _, gamma_neg = neg_params
             reg_out = self.reg_theta * self._cal_l2(alpha, beta_u, beta_pos, beta_neg, gamma_u, gamma_pos, gamma_neg)
@@ -100,7 +98,7 @@ class BPRLoss(nn.Module):
         return reg_out
 
     def forward(self, diff, pos_params, neg_params):
-        loss = -nn.functional.logsigmoid(diff).sum() # sigma(x_uij)
+        loss = -nn.functional.logsigmoid(diff).sum() # logsigma(x_uij)
         loss += self._reg_term(pos_params, neg_params) # reg_term
         return loss
     
@@ -126,7 +124,7 @@ class BPRMF(nn.Module):
         nn.init.xavier_uniform_(self.item_emb.weight.data)
     
     def cal_each(self, user, item):
-        mf_term = self.offset + self.user_bias(user).T + self.item_bias(item).T + ((self.user_emb(user)).matmul(self.item_emb(item).T)).sum(dim=1).unsqueeze(dim=0)
+        mf_term = self.offset + self.user_bias(user) + self.item_bias(item) + (self.user_emb(user)*self.item_emb(item)).sum(dim=1).unsqueeze(-1)
         params = (self.offset, self.user_bias(user), self.item_bias(item), self.user_emb(user), self.item_emb(item))
         return mf_term.squeeze(), params
     
