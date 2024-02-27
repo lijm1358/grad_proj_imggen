78925b078d3fc8e50135b8bd676cf43618b1a16c
lijm1358 <lijm1358@gmail.com>
2024-02-27 08:34:48 +0000

    feat: add fashion clip to diffusers

--- fashion_data_gen.py
+++ fashion_data_gen.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import os
 import pickle
+import argparse
 
 from diffusers import StableDiffusionPipeline, DPMSolverMultistepScheduler
 import torch
@@ -15,7 +16,7 @@ from fashion_clip.fashion_clip import FashionCLIP
 BATCH_SIZE = 4 
 WIDTH = 512
 HEIGHT = 512
-INFERENCE_STEP = 25
+INFERENCE_STEP = 30
 
 np.random.seed(42) # query 색상 랜덤 추가 시 seed 고정
 
@@ -26,13 +27,10 @@ cut_items = pd.read_csv("./VBPR/new_item_data.csv")
 items = items[items["article_id"].isin(cut_items["article_id"])]
 items_desc = items["detail_desc"].values
 items_id = items["article_id"].astype(str).values
-img_path_list = [os.path.join("0" + id[:2], "0" + id + ".jpg") for id in items_id]
 
-gen_img_save_path = "./dataset/images/"
-gen_latent_save_path = "./dataset/latents/"
-gen_embed_save_path = "./dataset/embeddings/"
-
-gen_log = "./dataset/logs.txt"
+gen_img_save_path = "./dataset_fashion/images/"
+gen_latent_save_path = "./dataset_fashion/latents/"
+gen_embed_save_path = "./dataset_fashion/embeddings/"
 
 
 def dump_pickle(data, path):
@@ -56,23 +54,27 @@ def query_add_color() -> List[str]:
                 
     return new_item_desc
 
-def main():
+def main(number_gpu):
+    gen_log = f"./dataset_fashion/logs_{number_gpu}.txt"
     new_item_desc = query_add_color()
     
     fclip = FashionCLIP('fashion-clip')
     
-    pipe = DiffusionPipeline.from_pretrained(
-        "runwayml/stable-diffusion-v1-5",
+    clip_model = CLIPModel.from_pretrained("patrickjohncyh/fashion-clip")
+    pipe = StableDiffusionPipeline.from_pretrained(
+        "stabilityai/stable-diffusion-2-1-base",
+        custom_pipeline="clip_guided_stable_diffusion",
         torch_dtype = torch.float16,
+        clip_model = clip_model,
         use_safetensors=True,
         safety_checker = None,
         requires_safety_checker = False,
     )
     pipe.scheduler = DPMSolverMultistepScheduler.from_config(pipe.scheduler.config)
 
-    pipe = pipe.to("cuda")
+    pipe = pipe.to(f"cuda:{number_gpu}")
     # pipe.enable_xformers_memory_efficient_attention()
-    pipe.unet = torch.compile(pipe.unet, mode="reduce-overhead", fullgraph=True)
+    # pipe.unet = torch.compile(pipe.unet, mode="reduce-overhead", fullgraph=True)
     
     for i, prompt in enumerate(new_item_desc):
         try:
@@ -86,16 +88,20 @@ def main():
             # images = pipe.image_processor.postprocess(images, output_type="pil")
             # torch.cuda.empty_cache()
             
-            images = pipe([prompt] * BATCH_SIZE, width=WIDTH, height=HEIGHT, num_inference_steps=INFERENCE_STEP).images
-            latents = pipe.latents
+            prompt += "Displayed against a white background, ensuring the entire item is visible and centrally placed. No one wears it."
             
-            image_embeddings = fclip.encode_images(images, batch_size=4)
+            images = pipe(prompt, width=WIDTH, height=HEIGHT, num_inference_steps=INFERENCE_STEP, guidance_scale=8.5)[0]
+            print(images)
+            # latents = pipe.latents
+            
+            # image_embeddings = fclip.encode_images(images, batch_size=4)
             
             os.makedirs(os.path.join(gen_img_save_path, filename), exist_ok=True)
-            torch.save(latents, os.path.join(gen_latent_save_path, f"{filename}.pt"))
+            # torch.save(latents, os.path.join(gen_latent_save_path, f"{filename}.pt"))
             for j, img in enumerate(images):
-                img.save(os.path.join(gen_img_save_path, filename, f"{filename}_{j}.jpg"))
-            dump_pickle(image_embeddings, os.path.join(gen_embed_save_path, f"{filename}.pkl"))
+                os.makedirs(os.path.join(gen_img_save_path, filename), exist_ok=True)
+                img.save(os.path.join(gen_img_save_path, filename, f"{filename}_{number_gpu}.jpg"))
+            # dump_pickle(image_embeddings, os.path.join(gen_embed_save_path, f"{filename}.pkl"))
             
             with open(gen_log, "a") as file:
                 file.write(f"{i} - {items_id[i]} : SUCCESS\n")
@@ -107,8 +113,14 @@ def main():
         
 
 if __name__ == '__main__':
+    # nohup python -u fashion_data_gen.py &
+    # tail -f nohup.out
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-n", "--number_gpu", dest="number_gpu", action="store")
+    args = parser.parse_args()
+    
     os.makedirs(gen_img_save_path, exist_ok=True)
     os.makedirs(gen_latent_save_path, exist_ok=True)
     os.makedirs(gen_embed_save_path, exist_ok=True)
     
-    main()
+    main(args.number_gpu)
