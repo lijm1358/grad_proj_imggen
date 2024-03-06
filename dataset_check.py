c941e1100a3f176079fe988da558cd42c95c2011
lijm1358 <lijm1358@gmail.com>
2024-03-06 16:05:27 +0000

    feat: add embedding test for dataset

--- dataset_check.py
+++ dataset_check.py
@@ -1,10 +1,24 @@
 import os 
 import pandas as pd
+import numpy as np
+from fashion_clip.fashion_clip import FashionCLIP
+import random
+import torch
 
 DATASET_PATH = "./dataset_fashion"
 IMAGE_PATH = os.path.join(DATASET_PATH, "images")
+EMBEDDING_PATH = os.path.join(DATASET_PATH, "embeddings")
 # CSV_PATH = "./data/articles_with_img.csv"
 CSV_PATH = "./data/new_item_data.csv"
+
+def seed_everything(seed: int = 42):
+    random.seed(seed)
+    np.random.seed(seed)
+    os.environ["PYTHONHASHSEED"] = str(seed)
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed(seed)  # type: ignore
+    torch.backends.cudnn.deterministic = True  # type: ignore
+    torch.backends.cudnn.benchmark = True  # type: ignore
 
 def main():
     img_original_df = pd.read_csv(CSV_PATH)
@@ -24,6 +38,36 @@ def main():
     except AssertionError:
         raise AssertionError(f"{target_path} has under 3 images")
     
+    
+    try:
+        original_ids = img_original_df["article_id"].sort_values().to_numpy()
+        gen_ids = sorted([int(id) for id in img_dir_list])
+
+        assert np.all(np.equal(original_ids, gen_ids))
+    except AssertionError:
+        raise AssertionError(f"Original item ids and generated item ids does not match.")
+    
+    try:        
+        target_ids = random.sample(img_dir_list, 10)
+        
+        print(f"Checking for 10 embeddings : {target_ids}")
+        
+        seed_everything(42)
+        fclip = FashionCLIP('fashion-clip')
+        
+        for target_id in target_ids:
+            original_emb = torch.load(os.path.join(EMBEDDING_PATH, target_id + ".pth"))
+            original_emb = torch.from_numpy(original_emb)
+            
+            target_img_group = [os.path.join(IMAGE_PATH, target_id, target_id + f"_{i}.jpg") for i in range(1, 4)]
+            
+            target_embedding = fclip.encode_images(target_img_group, batch_size=3)
+            target_embedding = torch.from_numpy(target_embedding)
+            
+            assert torch.allclose(original_emb, target_embedding, rtol=1e-4, atol=1e-5)
+    except AssertionError:
+        print(f"generated embedding and original embedding is not same.")
+        
     
 
 if __name__ == '__main__':
