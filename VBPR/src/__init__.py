448ea43690e8861dbb1ceec9b7e7991b19de1f45
lijm1358 <lijm1358@gmail.com>
2024-04-28 03:26:09 +0000

    remove unncecessary files

--- VBPR/src/__init__.py
+++
@@ -1,4 +0,0 @@
-from .model import VBPR, BPRLoss
-from .train import train, eval
-from .utils import seed_everything, get_config, get_timestamp, EarlyStopper, load_pickle, dump_pickle, mk_dir
-from .dataset import HMTestDataset, HMTrainDataset
